60a5d6e73d448fb97f77486a30353dda4ff76fc2
panshak <panshakeden@gmail.com>
2025-02-11 13:22:26 +0100

    recursion methods

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,28 @@ impl<'a> Parser<'a> {
 
 
     fn term(&mut self) -> i32 {
-   
+        let mut value = self.factor();
+        while self.match_token('*') {
+            value +=self.factor();
+        }
+        value
+    }
+
+    fn factor(&mut self) ->i32{
+        if self.match_token('('){
+            let value =self.expr();
+            self.match_token(')');
+            value
+        }else{
+            self.number()
+        }
+    }
+
+    fn number(&mut self) -> i32{
+        let digits =self.tokens[self.pos].to_digit(10).unwrap();
+        self.pos +=1;
+
+        digits as i32
     }
 
 
