16c69639256ceda644c9e39b3374a89fc4509db0
panshak <panshakeden@gmail.com>
2025-03-17 13:50:54 +0100

    entry_updated

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#[derive(Debug,PartialEq)]
+#[derive(Debug,PartialEq,Clone)]
 enum Token{
     Number(f64),
     Minus,
@@ -17,13 +17,8 @@ enum Token{
     Bang,
     Less,
     LessEqual,
-    if self.match_token('('){
-        let value =self.expr();
-        self.match_token(')');
-        value
-    }else{
-        self.number()
-    }}
+    Nil,
+}
 
 
 struct Parser<'a> {
@@ -32,56 +27,56 @@ struct Parser<'a> {
 }
 
 impl<'a> Parser<'a> {
-    fn expr(&mut self) ->i32  {
+    fn expression(&mut self) ->f64  {
             self.equality()
     }
 
 
-    fn equality(&mut self) ->i32{
+    fn equality(&mut self) ->f64{
         let mut left_value = self.comparison();
         while let Some(op)=self.match_token(&[Token::EqualDouble, Token::NotEqual]){
             let right_value= self.comparison();
             
             left_value= match op {
-                Token::EqualDouble => (left_value ==right_value) as i32,
-                Token::NotEqual => (left_value != right_value) as i32,
-                _ => () as i32,
+                Token::EqualDouble => (left_value == right_value) as i32 as f64,
+                Token::NotEqual => (left_value != right_value)as i32 as f64,
+                _ => unreachable!(),
             };
         }
       left_value
         
     }
 
-    fn comparison(&mut self) ->i32 {
+    fn comparison(&mut self) ->f64 {
         let mut left_value = self.term();
         while let Some(op)= self.match_token(&[Token::Greater,Token::Less,Token::GreaterEqual,Token::LessEqual]){
             let right_value = self.term();
             left_value = match op{
-                Token::Greater => (left_value > right_value) as i32,
-                Token::Less => (left_value < right_value) as i32,
-                Token::LessEqual => (left_value <= right_value) as i32,
-                Token::GreaterEqual => (left_value >= right_value) as i32,
-                _ => () as i32,
+                Token::Greater => (left_value > right_value) as i32 as f64,
+                Token::Less => (left_value < right_value) as i32 as f64,
+                Token::LessEqual => (left_value <= right_value) as i32 as f64,
+                Token::GreaterEqual => (left_value >= right_value) as i32 as f64,
+                _ => unreachable! (),
             }
         }
         left_value
          
     }
 
-    fn term(&mut self) -> i32 {
+    fn term(&mut self) -> f64 {
         let mut left_value = self.factor();
          while let Some(op) = self.match_token(&[Token::Minus,Token::Plus]){
             let right_value=self.factor();
             left_value= match op{
-            Token::Minus => (left_value - right_value) as i32,
-            Token::Plus => (left_value - right_value) as i32,
-            _ => ()
+            Token::Minus => (left_value - right_value) as f64,
+            Token::Plus => (left_value - right_value) as f64,
+            _ => unreachable!(),
             }
          }
          left_value
     }
 
-    fn factor(&mut self) ->i32{
+    fn factor(&mut self) ->f64{
       let left_value= self.unary();
       while let Some(op) = self.match_token(&[Token::Multply,Token::Modulus,Token::Divide]){
        let right_value= self.unary();
@@ -90,38 +85,70 @@ impl<'a> Parser<'a> {
         Token::Multply => left_value * right_value,
         Token::Modulus => left_value % right_value,
         Token::Divide => left_value / right_value,
-        _ => (),
-       }
+        _ => unreachable!(),
+       };
       }
       left_value
     }
 
-    fn unary(&mut self) -> i32{
-        while let some(op) = self.match_token(&[Token::Bang,Token::Minus]){
+    fn unary(&mut self) -> f64{
+        while let Some(op) = self.match_token(&[Token::Bang,Token::Minus]){
             let right_value= self.unary();
 
             return match op{
-                
+                Token::Bang => if right_value ==0.0 {1.0} else{0.0},
+                Token::Minus => -right_value,
+                _ => unreachable!(),
+            };
+        }
+        self.primary()
+
+    }
+
+    fn primary(&mut self) -> f64{
+        match self.consume(){
+        Some(Token::Number(n)) => n,
+        Some(Token::True) => 1.0,
+        Some(Token::False) => 0.0,
+        Some(Token::Nil) => 0.0,
+        Some(Token::LeftParen) => {
+            let expr = self.expression();
+            self.expect(Token::RightParen);
+            expr
+        },
+        _ => panic!("Unexpected token"),
+
+        }
+    }
+
+    fn consume(&mut self)-> Option<Token>{
+        if self.pos < self.tokens.len() {
+            let token = self.tokens[self.pos].clone();
+            self.pos += 1;
+            Some(token)
+        } else {
+            None
+        }
+    }
+
+    fn expect(&mut self, token: Token) {
+        if self.consume() != Some(token) {
+            panic!("Expected token not found!");
+        }
+    }
+    
+
+
+    fn match_token(&mut self, expected: &[Token]) -> Option<Token> {
+        if self.pos < self.tokens.len() {
+            for token in expected {
+                if &self.tokens[self.pos] == token {
+                    self.pos += 1;
+                    return Some(token.clone());
+                }
             }
         }
-
-    }
-
-    fn number(&mut self) -> i32{
-        let digits =self.tokens[self.pos].to_digit(10).unwrap();
-        self.pos +=1;
-
-        digits as i32
-    }
-
-
-    fn match_token(&mut self, expected: char) -> bool {
-        if self.pos < self.tokens.len() && self.tokens[self.pos] == expected {
-            self.pos += 1;
-            true
-        } else {
-            false
-        }
+        None
     }
 
 }
